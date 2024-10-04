f8dcaa09c81365c88e095e75c3836ef2be655968
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-10-04 11:57:18 +0100

    scaled gun function

--- bonus/gun_bonus.c
+++ bonus/gun_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aconceic <aconceic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:53:42 by aconceic          #+#    #+#             */
-/*   Updated: 2024/10/04 11:44:34 by aconceic         ###   ########.fr       */
+/*   Updated: 2024/10/04 11:56:22 by aconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,45 +30,29 @@ void	init_gun_struct(t_gm *game)
 
 void draw_scaled_gun(t_gm *game, int scale_factor, int img_i)
 {
-    int orig_x;
-    int orig_y;
-    int new_x;
-    int new_y;
-    int start_x;
-    int start_y;
+    t_gun *g;
     int color;
-    int scaled_gun_w;
-    int scaled_gun_h;
 
-    // Calculate the new dimensions of the scaled gun
-    scaled_gun_w = game->textr->txt_width[img_i] * scale_factor;
-    scaled_gun_h = game->textr->txt_height[img_i] * scale_factor;
-
-    // Calculate the starting position for the scaled gun
-    start_x = (WIDTH - scaled_gun_w) / 2;
-    start_y = (HEIGHT - game->gun->move) - scaled_gun_h;
-    new_y = 0;
-    // Loop through each pixel in the scaled gun sprite
-    while (new_y < scaled_gun_h)
+    g = game->gun;
+    g->scaled_h = game->textr->txt_height[img_i] * scale_factor;
+    g->scaled_w = game->textr->txt_width[img_i] * scale_factor;
+    g->start_x = (WIDTH - g->scaled_w) / 2;
+    g->start_y = (HEIGHT - game->gun->move) - g->scaled_h;
+    g->new_y = 0;
+    while (g->new_y < g->scaled_h) // Loop through each pixel in the scaled gun sprite
     {
-        new_x = 0;
-        while (new_x < scaled_gun_w)
+        g->new_x = 0;
+        while (g->new_x < g->scaled_w)
         {
-            orig_x = new_x / scale_factor;
-            orig_y = new_y / scale_factor;
-
-            // Get the pixel color from the original gun image
-            color = my_mlx_get_pixel(game, orig_x, orig_y, img_i);  // Assuming 4 is the gun texture index
-
-            // Skip transparent pixels (assuming transparency is represented by 0)
-            if ((color & 0x00FFFFFF) != 0) // Only draw non-transparent pixels
-            {
-                // Draw the scaled pixel on the screen
-                my_mlx_pixel_put(game->mlx, start_x + new_x, start_y + new_y, color);
-            }
-            new_x ++;
+            g->orig_x = g->new_x / scale_factor;
+            g->orig_y = g->new_y / scale_factor;
+            color = my_mlx_get_pixel(game, g->orig_x, g->orig_y, img_i);
+            if ((color & 0x00FFFFFF) != 0) //only prints if not transparent
+                my_mlx_pixel_put(game->mlx, g->start_x + g->new_x,
+                                    g->start_y + g->new_y, color);
+            g->new_x ++;
         }
-        new_y++;
+        g->new_y++;
     }
 }
 
